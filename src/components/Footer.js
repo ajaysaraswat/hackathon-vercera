fca20230a88d61a4319fcc9f85c6e4e15f768968
AJAY KUMAR SARASWAT <ajaykumarsaraswat9045@gmail.com>
2024-03-03 21:04:38 +0530

    t commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Box, Stack, Typography } from '@mui/material';
-import Logo from '../assets/images/Logo-1.png';
+import Logo from '../assets/images/exer.jpg';
 
 const Footer = () => (
-  <Box mt="80px" bgcolor="#FFF3F4">
+  
+  <Box mt="80px" bgcolor="skyblue">
     <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
-      <img src={Logo} alt="logo" style={{ width: '300px', height: '150px' }} />
+      <img src={Logo} alt="logo" style={{ width: '100%', height: '350px' }} />
     </Stack>
-    <Typography variant="h5" sx={{ fontSize: { lg: '40px', xs: '20px' } }} mt="5px" textAlign="center" pb="40px">Sweat + Sacrifice = Success</Typography>
+    <Typography variant="h5" sx={{ fontSize: { lg: '40px', xs: '20px' } }} mt="5px" textAlign="center" pb="40px" bgcolor={"red"}>Sweat + Sacrifice = Success</Typography>
+    <Typography textAlign="center">copyright@vercera 2024</Typography>
   </Box>
+ 
 );
+
 
 export default Footer;
