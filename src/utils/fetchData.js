95f0a8a7a51e5115e7dca7643a29a7c608216728
AJAY KUMAR SARASWAT <ajaykumarsaraswat9045@gmail.com>
2024-03-03 20:21:18 +0530

    Initial commit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,23 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': 'd78a0a3e61msh8e330fc5877e02cp10ae26jsn7a9e84c98b93',
+    //extra api key: b8970715c9msh3e4295fc85b2844p1ef953jsndb3c2716ec0d 
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
